f488580c1f07fa531d241b35026cae9b8957dda5
NikitOs-ux <61090706+NikitOs-ux@users.noreply.github.com>
2020-05-28 01:48:19 +0700

    day 2

--- script/main.js
+++ script/main.js
@@ -1,14 +1,5 @@
-const DBservice = class {
-	getData = (url)=>{
-		const res = fetch(url);
-		console.log(res);
-	}
-	getTestData = ()=>{
-		return this.getData('test.json')
-	}
-}
-
-console.log(new DBservice().getTestData());
+const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+const API_KEY = '42ff8c58d1f50c43406409cb9167e68f';
 
 
 
@@ -16,6 +7,74 @@ const leftMenu= document.querySelector('.left-menu');
 const hanburger =document.querySelector('.hamburger');
 const tvShowsList=document.querySelector('.tv-shows__list');
 modal =document.querySelector('.modal');
+
+
+
+
+const DBservice = class {
+	getData =async (url)=>{
+		const res = await fetch(url);
+		console.log(res);
+		if (res.ok){
+			return res.json();
+
+		}else{
+			throw new Error(`не удалось получить данные по адресу ${url}`)
+		}
+	}
+
+	getTestData = ()=>{
+		return this.getData('test.json')
+	}
+}
+const renderCard = response =>{
+	console.log(response);
+	tvShowsList.textContent = '';
+	
+
+	response.results.forEach(({
+								backdrop_path: backdrop,
+								name: title,
+								poster_path:poster,
+								vote_average:vote
+								})=>{
+
+		const posterIMG =poster ? IMG_URL + poster : 'img/no-poster.jpg';
+		const backdropIMG ='';
+		const voidElem = '';
+
+		const card = document.createElement('li');
+		card.className = 'tv-shows__item';
+		card.innerHTML = `
+		<a href="#" class="tv-card">
+			<span class="tv-card__vote">${vote}</span>
+			<img class="tv-card__img"
+				src="${posterIMG}"
+				data-backdrop="${IMG_URL + backdrop}"
+				alt="${title}">
+			<h4 class="tv-card__head">${title}</h4>
+		</a>
+		`;
+		 tvShowsList.append(card);
+	});
+
+
+}
+new DBservice().getTestData().then(renderCard);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // open/close menu
 
 hanburger.addEventListener('click', ()=>{
@@ -77,10 +136,7 @@ const changeImage = event =>{
 tvShowsList.addEventListener('mouseover', changeImage);
 tvShowsList.addEventListener('mouseout', changeImage);
 
-
-
-
-const mike = new human('Mike', 43);
-const alex =new human('Alex',15)
-console.log(mike);
-console.log(alex);
+// const mike = new human('Mike', 43);
+// const alex =new human('Alex',15)
+// console.log(mike);
+// console.log(alex);
